e7e64dfb56c5cb396e812ab50e250ffe8306ca11
Michael <moappel@cmu.edu>
2015-10-29 19:58:07 -0400

    initial javascript work

--- viz/viz.js
+++ viz/viz.js
@@ -1,3 +1,57 @@
-var dataset = ["app", "untrusted_app", "system_server", "zygote"];
+var width=640;
+var height=480;
+
+var nodes = [];
+var links = [];
+
+console.log("Working");
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
 
+var link = svg.selectAll(".link")
+    .data(links)
+    .enter().append("line")
+    .attr("class", "link");
+
+var node = svg.selectAll('.node')
+    .data(nodes)
+    .enter().append('circle')
+    .attr('class', 'node');
+
+
+//d3.csv("data/test.csv", function(data) {
+
+function ready(error, data) {
+    var force = d3.layout.force().size([width, height])
+        .nodes(nodes)
+        .links(links);
+
+    force.on("end", function() {
+        node.attr('r', width/25)
+            .attr('cx', function(d) { return d.x; })
+            .attr('cy', function(d) { return d.y; });
+         link.attr('x1', function(d) { return d.source.x; })
+             .attr('y1', function(d) { return d.source.y; })
+             .attr('x2', function(d) { return d.target.x; })
+             .attr('y2', function(d) { return d.target.y; });
+    });
+
+    force.start();
+}
+
+queue()
+    .defer(d3.csv, "data/test.csv", function(data) {
+        var src_idx = 0;
+        var dst_idx = 0;
+        console.log("Parsing csv: " + data.length);
+        data.forEach(function(d) {
+            src_idx = nodes.push(d.domain) - 1;
+            dst_idx = nodes.push(d.type) - 1;
+            console.log("Nodes: " + src_idx + ", " + dst_idx);
+            links.push({source:src_idx, target:dst_idx});
+        });
+    })
+    .await(ready);
