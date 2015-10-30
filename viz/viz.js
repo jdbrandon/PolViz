a802f9341cf5bb73b58a63937ccff3918854fe69
Michael <moappel@cmu.edu>
2015-10-29 20:07:19 -0400

    Fixed csv parsing

--- viz/viz.js
+++ viz/viz.js
@@ -21,37 +21,37 @@ var node = svg.selectAll('.node')
     .enter().append('circle')
     .attr('class', 'node');
 
+queue()
+    .defer(d3.csv, "data/test.csv")
+    .await(analyze);
 
-//d3.csv("data/test.csv", function(data) {
-
-function ready(error, data) {
-    var force = d3.layout.force().size([width, height])
-        .nodes(nodes)
-        .links(links);
-
-    force.on("end", function() {
-        node.attr('r', width/25)
-            .attr('cx', function(d) { return d.x; })
-            .attr('cy', function(d) { return d.y; });
-         link.attr('x1', function(d) { return d.source.x; })
-             .attr('y1', function(d) { return d.source.y; })
-             .attr('x2', function(d) { return d.target.x; })
-             .attr('y2', function(d) { return d.target.y; });
+function analyze(error, data) {
+    console.log(data[0]);
+    var src_idx = 0;
+    var dst_idx = 0;
+    console.log("Parsing csv: " + data.length);
+    data.forEach(function(d) {
+        src_idx = nodes.push(d.domain) - 1;
+        dst_idx = nodes.push(d.type) - 1;
+        console.log("Nodes: " + src_idx + ", " + dst_idx);
+        links.push({source:src_idx, target:dst_idx});
     });
-
-    force.start();
 }
 
-queue()
-    .defer(d3.csv, "data/test.csv", function(data) {
-        var src_idx = 0;
-        var dst_idx = 0;
-        console.log("Parsing csv: " + data.length);
-        data.forEach(function(d) {
-            src_idx = nodes.push(d.domain) - 1;
-            dst_idx = nodes.push(d.type) - 1;
-            console.log("Nodes: " + src_idx + ", " + dst_idx);
-            links.push({source:src_idx, target:dst_idx});
-        });
-    })
-    .await(ready);
+var force = d3.layout.force().size([width, height])
+    .nodes(nodes)
+    .links(links);
+
+force.on("end", function() {
+    console.log("Force End");
+    node.attr('r', width/25)
+        .attr('cx', function(d) { return d.x; })
+        .attr('cy', function(d) { return d.y; });
+     link.attr('x1', function(d) { return d.source.x; })
+         .attr('y1', function(d) { return d.source.y; })
+         .attr('x2', function(d) { return d.target.x; })
+         .attr('y2', function(d) { return d.target.y; });
+});
+
+console.log("Force Start");
+force.start();
